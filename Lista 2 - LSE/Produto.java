d77ac5c1827dc0f5b16d87a9e7ae95516f44a0ab
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-03-14 14:53:40 -0300

    Create Produto.java

---
+++ Lista 2 - LSE/Produto.java
@@ -0,0 +1,45 @@
+package atividade2;
+/*
+ * @author valmir
+ */
+public class Produto implements Comparable <Produto>{//compara se tem iguala
+    private String codigo;
+    private String descricao;
+    private double preco;
+    private int estoque;
+
+    public Produto (String codigo, String descricao, double preco, int estoque){
+        this.codigo= codigo;
+        this.descricao= descricao;
+        this.preco= preco;
+        this.estoque= estoque;
+    }
+    public void setCodigo(String codigo){
+        this.codigo= codigo;
+    } 
+    public String getCodigo (){
+        return this.codigo;
+    }
+    public void setDescricao(){
+        this.descricao= descricao;
+    }
+    public String getdescriacao(){
+        return this.descricao;
+    }
+    public void setPreco(double preco){
+        this.preco= preco;
+    }
+    public double getPreco(){
+        return this.preco= preco;
+    }
+    public void setEstoque(){
+        this.estoque= estoque;
+    }
+    @Override
+    public int compareTo(Produto p) {
+        return this.codigo.compareTo(codigo);
+    }
+    public String toString(){
+        return "O Codigo: "+ this.codigo+ ", descrição: "+ this.descricao+ ", do preço: "+ this.preco+ ", possui no estoque: "+this.estoque+ "quantidades";
+    }
+}
