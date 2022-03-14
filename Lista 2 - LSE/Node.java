552b5274a9dc7b51510d32e1d3ed5312dc4cb4a8
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-03-14 14:55:12 -0300

    Create Node.java

---
+++ Lista 2 - LSE/Node.java
@@ -0,0 +1,24 @@
+package atividade2;
+/*
+ * @author valmir
+ */
+ public class Node{
+    private Produto info;
+    private Node prox;
+    
+    public Node(Produto p){
+        this.info= p;
+    }
+    public void setInfo(Produto p){
+        this.info= p;
+    }
+    public Produto getInfo(){
+        return this.info;
+    }
+   public void setProx(Node novoProx){
+        this.prox= novoProx;
+    }
+    public Node getProx(){
+        return this.prox;
+    }
+}
