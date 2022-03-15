77e1391d3a1d46f0ed26faaa49db271f5500117e
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-03-15 14:32:06 -0300

    Update aplicação.java

--- Lista 2 - LSE/aplicação.java
+++ Lista 2 - LSE/aplicação.java
@@ -1,37 +1,63 @@
-package atividade2;
+package lselista2;
 import java.util.Scanner;
-/*
- * @author valmir
- */
-public class Atividade2 {
+public class LSELista2 {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
+        LSEComDescritor lista2= new LSEComDescritor();
         int escolha;
+        String codigo, descricao;
+        double preco;
+        int estoque;
+        Produto p;
         do{
             menu();
             System.out.print("Escolha um opção acima: ");
             escolha= in.nextInt();
-            //colocar aqui
             switch (escolha){
                 case 1: 
+                    in.nextLine();
+                    System.out.print("Digite o código do produto: ");
+                    codigo= in.nextLine();
+                    System.out.print("Digite a descricao do produto: ");
+                    descricao= in.nextLine();
+                    System.out.print("Digite o preço do produto: ");
+                    preco= in.nextDouble();
+                    System.out.print("Digite a quantidade presente no estoque: ");
+                    estoque= in.nextInt();
+                    p= new Produto(codigo,descricao,preco,estoque);
+                    lista2.InserirInicio(p);
                     break;
                     
-                case 2:
+                case 2://remoção no fim da lista
+                    lista2.RemoverInicio();
                     break;
                     
                 case 3:
+                    in.nextLine();
+                    System.out.print("Digite o código do produto: ");
+                    codigo= in.nextLine();
+                    System.out.print("Digite a descricao do produto: ");
+                    descricao= in.nextLine();
+                    System.out.print("Digite o preço do produto: ");
+                    preco= in.nextDouble();
+                    System.out.print("Digite a quantidade presente no estoque: ");
+                    estoque= in.nextInt();
+                    p= new Produto(codigo,descricao,preco,estoque);
+                    lista2.InserirFinal(p);
                     break;
                     
-                case 4:
+                case 4://remoção no fim da lista
+                    lista2.RemoverFinal();
                     break;
                     
-                case 5:
+                case 5://exibir tudo
+                    lista2.exibirTudo();
                     break;
                     
-                case 6:
+                case 6://exibir dados de um único produto
                     break;
                     
-                case 7:
+                case 7://deve fazer uma busca com o codigo, pra assim poder alterar
                     break;
                     
                 case 0:
@@ -46,13 +72,14 @@ public class Atividade2 {
     }
 public static void menu(){
     System.out.println("Menu de opções");
-    System.out.println("1 - Inserir um novo aluno no início da lista um novo aluno"); //feito em aula
-    System.out.println("2 - Remover o primeiro aluno da lista");
-    System.out.println("3 - Inserir um novo aluno no final da lista");
-    System.out.println("4 - Remover último aluno da lista");
-    System.out.println("5 - Exibir todos alunos da lista");//feito em aula
-    System.out.println("6- Exibir os dados de um produto da lista");
-    System.out.println("7- Alterar os dados de um produto da lista");
-    System.out.println("0 - Sair do programa");
+    System.out.println("1 - Inserir um novo produto no início da lista"); //feito em aula
+    System.out.println("2 - Remover o primeiro produto da lista");
+    System.out.println("3 - Inserir um novo produto no final da lista");
+    System.out.println("4 - Remover último produto da lista");
+    System.out.println("5 - Exibir todos produto da lista");//feito em aula
+    System.out.println("6 - Exibir os dados de um produto da lista");
+    System.out.println("7 - Alterar os dados de um produto da lista");
+    System.out.println("0 - Sair do programa!");
 }
-}
+
+} 
